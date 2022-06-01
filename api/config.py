aeaea2bf7859868e26452cecd54ef1472e41d9b6
niek318 <niek.muijs@gmail.com>
2022-06-01 19:35:23 +0200

    config

--- api/config.py
+++ api/config.py
@@ -7,4 +7,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'niekniek'
 app.config['MYSQL_DATABASE_DB'] = 'button_logging'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3308
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
 mysql.init_app(app)
