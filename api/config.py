2597967ec44969c37646ade1a47956beae5624e3
niek318 <niek.muijs@gmail.com>
2022-06-02 14:13:56 +0200

    added database

--- api/config.py
+++ api/config.py
@@ -2,15 +2,11 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'niekniek'
-app.config['MYSQL_DATABASE_DB'] = 'button_logging'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_PORT'] = 3308
+app.config['MYSQL_DATABASE_USER'] = 'sql11497267'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'K6NWAtiWkH'
+app.config['MYSQL_DATABASE_DB'] = 'sql11497267'
+app.config['MYSQL_DATABASE_HOST'] = 'sql11.freemysqlhosting.net'
+app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['CORS_ALLOW_ORIGIN'] = "*,*"
-app.config['CORS_EXPOSE_HEADERS'] = "*,*"
-app.config['CORS_ALLOW_HEADERS'] = "content-type,*"
-
 mysql.init_app(app)
